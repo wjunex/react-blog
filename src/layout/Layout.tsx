18c6b5aed02a1acca101323e685268e7b08115ba
wjunxxx <wjun@88.com>
2026-05-20 22:54:06 +0800

    feat: 优化返回顶部

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,16 +1,18 @@
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
+import BackToTop from "@/components/BackToTop";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="sm:min-h-screen sm:px-6 sm:py-5 lg:py-10">
-      <div className="mx-auto flex min-h-screen w-full max-w-5xl flex-col sm:overflow-hidden sm:min-h-[calc(100vh-40px)] sm:rounded-2xl sm:border sm:border-(--border) sm:bg-(--surface) sm:shadow-[var(--shadow)] lg:min-h-[calc(100vh-80px)]">
+      <div className="mx-auto flex min-h-screen w-full max-w-5xl flex-col sm:overflow-hidden sm:min-h-[calc(100vh-40px)] sm:rounded-2xl sm:border sm:border-(--border) sm:bg-(--surface) sm:shadow-(--shadow) lg:min-h-[calc(100vh-80px)]">
         <NavBar />
         <main className="flex-1 px-5 py-8 sm:px-8 lg:px-14 lg:py-12">
           <div className="mx-auto w-full max-w-3xl">{children}</div>
         </main>
         <Footer />
       </div>
+      <BackToTop />
     </div>
   );
 }
